2c6892be948c2425dd772c0e8bbaf0cd890901b8
datapirateX <67560590+datapirateX@users.noreply.github.com>
2023-08-08 00:35:39 +0600

    ls

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 
 const Container = styled.div`
-  height: 60vh;
+  height: 40vh;
   background-color: #fcf5f5;
   display: flex;
   align-items: center;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Announcement from '../components/Announcement'
-import Slider from '../components/Slider'
-import Categories from '../components/Categories'
-import Products from '../components/Products'
-import Footer from '../components/Footer'
-import Newsletter from '../components/Newsletter'
-import ProductList from './ProductList'
+import React from "react";
+import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
-      <ProductList />
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Products/>
+      <Newsletter/>
+      <Footer/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
